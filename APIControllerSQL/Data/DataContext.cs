953bf415a277c3137b1ed492f506802491d34411
LiamEngelin <liam.engelin@iths.se>
2025-02-09 17:00:33 +0100

    Add project files.

---
+++ APIControllerSQL/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using APIControllerSQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIControllerSQL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Superhero> Superheroes { get; set; }
+    }
+}
---
+++ APIControllerSQL/Models/Superhero.cs
@@ -0,0 +1,11 @@
+﻿namespace APIControllerSQL.Models
+{
+    public class Superhero
+    {
+        public int Id {  get; set; }
+        public string HeroName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Origin { get; set; }
+    }
+}
